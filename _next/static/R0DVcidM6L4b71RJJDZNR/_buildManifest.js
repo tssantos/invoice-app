760d71c19bd1563ee4ff603407c331b5954bcbde
tssantos <tssantos@gmail.com>
2022-05-31 15:19:29 +0000

    Deploying to gh-pages from @ tssantos/invoice-app@cd8d3f6f0178c7e4d13b45ef6579cf55240ad251 🚀

---
+++ _next/static/R0DVcidM6L4b71RJJDZNR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a39740b604fb1ae6.css","static/chunks/pages/index-bd6205f6c50d5482.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
